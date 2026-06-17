dfcaa735a627b50474d8d9d7ae5148bf52c6df0e
sbudani-cloud <sbudani@alumno.huergo.edu.ar>
2026-06-17 10:24:33 -0300

    md1

--- md1.py
+++ md1.py
@@ -1,5 +1,6 @@
 import json
 
+#clases
 class Pokemon:
     def __init__(self, id, nombre, tipo, pc):
         self.id = id
@@ -63,6 +64,54 @@ class HashMap:
 
         print(f"Clave '{key}' no encontrada.")
 
+class HashSet:
+    def __init__(self):
+        self.capacidad = 10
+        self.buckets = [[] for _ in range(self.capacidad)]
+
+    def _hash(self, key):
+        return hash(key) % self.capacidad
+
+    def agregar(self, key):
+        indice = self._hash(key)
+        bucket = self.buckets[indice]
+
+        if key not in bucket:
+            bucket.append(key)
+            print(f"Elemento '{key}' agregado.")
+        else:
+            print(f"El elemento '{key}' ya existe.")
+
+    def eliminar(self, key):
+        indice = self._hash(key)
+        bucket = self.buckets[indice]
+
+        if key in bucket:
+            bucket.remove(key)
+            print(f"Elemento '{key}' eliminado.")
+        else:
+            print(f"Elemento '{key}' no encontrado.")
+
+    def buscar(self, key):
+        indice = self._hash(key)
+        bucket = self.buckets[indice]
+
+        return key in bucket
+
+    def modificar(self, key_vieja, key_nueva):
+        if not self.buscar(key_vieja):
+            print(f"El elemento '{key_vieja}' no existe.")
+            return
+
+        if self.buscar(key_nueva):
+            print(f"El elemento '{key_nueva}' ya existe.")
+            return
+
+        self.eliminar(key_vieja)
+        self.agregar(key_nueva)
+        print(f"Elemento '{key_vieja}' modificado a '{key_nueva}'.")
+
+#---cargar
 def cargar_pokedex(ruta_archivo, pokedex):
     with open(ruta_archivo, "r", encoding="utf-8") as file:
         datos = json.load(file)
@@ -76,6 +125,15 @@ def cargar_pokedex(ruta_archivo, pokedex):
         )
 
         pokedex.agregar(pokemon.id, pokemon)
-
 Pokedex = HashMap()
 cargar_pokedex("pokedex.json", Pokedex)
+
+medallasObtenidas = []
+def cargar_medallas(ruta_archivo):
+    with open(ruta_archivo, "r", encoding="utf-8") as file:
+        datos = json.load(file)
+
+    medallasObtenidas.append(datos[0])
+    medallasObtenidas.append(datos[1])
+cargar_medallas("medallas.json")
+print(medallasObtenidas)
